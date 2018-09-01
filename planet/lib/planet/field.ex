8ae841ae0d5e2b3396df40ee16b55f8249e6b18e
Will Shown <w@willshown.com>
2018-09-01 00:41:23 -0500

    Add linking tests

--- planet/lib/planet/field.ex
+++ planet/lib/planet/field.ex
@@ -7,7 +7,7 @@ defmodule PLANET.Field do
 
   @sections 5
 
-  @doc "Provides a list of indices for a field's adjacent fields."
+  @doc "Provides a map of indices for a field's adjacent fields."
   def adjacents(:north, _) do
     %{
       nw: {:sxy, 0, 0, 0},
@@ -43,6 +43,7 @@ defmodule PLANET.Field do
 
     is_pentagon = y == 0 and rem(x + 1, d) == 0
 
+    # northwestern adjacent (x--)
     adj_nw =
       cond do
         x > 0 -> {:sxy, s, x - 1, y}
@@ -50,22 +51,30 @@ defmodule PLANET.Field do
         true -> {:sxy, prev_s, y - 1, 0}
       end
 
+    # western adjacent (x--, y++)
     adj_w =
       cond do
+        # attach northwestern edge to previous north-northeastern edge
         x == 0 -> {:sxy, prev_s, y, 0}
+        # attach southwestern edge to previous southeastern edge
         y == max_y and x > d -> {:sxy, prev_s, max_x, x - d}
+        # attach southwestern edge to previous east-northeastern edge
         y == max_y -> {:sxy, prev_s, x + d - 1, 0}
         true -> {:sxy, s, x - 1, y + 1}
       end
 
+    # southwestern adjacent (y++)
     adj_sw =
       cond do
         y < max_y -> {:sxy, s, x, y + 1}
         x == max_x and y == max_y -> :south
+        # attach southwestern edge to previous southeastern edge
         x >= d -> {:sxy, prev_s, max_x, x - d + 1}
+        # attach southwestern edge to previous east-northeastern edge
         true -> {:sxy, prev_s, x + d, 0}
       end
 
+    # southeastern adjacent (x++)
     adj_se =
       cond do
         is_pentagon and x == d - 1 -> {:sxy, s, x + 1, 0}
@@ -74,21 +83,27 @@ defmodule PLANET.Field do
         true -> {:sxy, s, x + 1, y}
       end
 
+    # eastern adjacent (x++, y--)
     adj_e =
       cond do
         is_pentagon and x == d - 1 -> {:sxy, next_s, 0, max_y}
         is_pentagon and x == max_x -> {:sxy, next_s, d - 1, max_y}
         x == max_x -> {:sxy, next_s, y + d - 1, max_y}
+        # attach northeastern side to next northwestern edge
         y == 0 and x < d -> {:sxy, next_s, 0, x + 1}
+        # attach northeastern side to next west-southwestern edge
         y == 0 -> {:sxy, next_s, x - d + 1, max_y}
         true -> {:sxy, s, x + 1, y - 1}
       end
 
+    # northeastern adjacent (y--)
     adj_ne =
       cond do
         is_pentagon -> nil
         y > 0 -> {:sxy, s, x, y - 1}
+        # attach northeastern side to next northwestern edge
         y == 0 and x < d -> {:sxy, next_s, 0, x}
+        # attach northeastern side to next west-southwestern edge
         y == 0 -> {:sxy, next_s, x - d, max_y}
       end
 
---
+++ planet/test/field_test.exs
@@ -0,0 +1,104 @@
+defmodule PLANET.FieldTest do
+  use ExUnit.Case
+
+  import PLANET.Field
+
+  doctest PLANET.Field
+
+  test "links northern polar field correctly" do
+    d = 1
+    assert adjacents(:north, d).nw == {:sxy, 0, 0, 0}
+    assert adjacents(:north, d).w == {:sxy, 1, 0, 0}
+    assert adjacents(:north, d).sw == {:sxy, 2, 0, 0}
+    assert adjacents(:north, d).se == {:sxy, 3, 0, 0}
+    assert adjacents(:north, d).e == {:sxy, 4, 0, 0}
+    assert adjacents(:north, d).ne == nil
+  end
+
+  test "links southern polar field correctly" do
+    d = 3
+    dy = d
+    dx = 2 * d
+
+    assert adjacents(:south, d).nw == {:sxy, 0, dx - 1, dy - 1}
+    assert adjacents(:south, d).w == {:sxy, 1, dx - 1, dy - 1}
+    assert adjacents(:south, d).sw == {:sxy, 2, dx - 1, dy - 1}
+    assert adjacents(:south, d).se == {:sxy, 3, dx - 1, dy - 1}
+    assert adjacents(:south, d).e == {:sxy, 4, dx - 1, dy - 1}
+    assert adjacents(:south, d).ne == nil
+  end
+
+  test "links the northern tropical fields correctly" do
+    d = 1
+    index = {:sxy, 0, 0, 0}
+
+    assert adjacents(index, d).nw == :north
+    assert adjacents(index, d).w == {:sxy, 4, 0, 0}
+    assert adjacents(index, d).sw == {:sxy, 4, 1, 0}
+    assert adjacents(index, d).se == {:sxy, 0, 1, 0}
+    assert adjacents(index, d).e == {:sxy, 1, 0, 0}
+    assert adjacents(index, d).ne == nil
+  end
+
+  test "links the southern tropical fields correctly" do
+    d = 1
+    index = {:sxy, 0, 1, 0}
+
+    assert adjacents(index, d).nw == {:sxy, 0, 0, 0}
+    assert adjacents(index, d).w == {:sxy, 4, 1, 0}
+    assert adjacents(index, d).sw == :south
+    assert adjacents(index, d).se == {:sxy, 1, 1, 0}
+    assert adjacents(index, d).e == {:sxy, 1, 0, 0}
+    assert adjacents(index, d).ne == nil
+  end
+
+  test "links fields on the north-northeastern edge" do
+    d = 3
+    index = {:sxy, 0, 0, 0}
+
+    assert adjacents(index, d).nw == :north
+    assert adjacents(index, d).w == {:sxy, 4, 0, 0}
+    assert adjacents(index, d).sw == {:sxy, 0, 0, 1}
+    assert adjacents(index, d).se == {:sxy, 0, 1, 0}
+    assert adjacents(index, d).e == {:sxy, 1, 0, 1}
+    assert adjacents(index, d).ne == {:sxy, 1, 0, 0}
+  end
+
+  test "links fields on the east-northeastern edge" do
+    d = 3
+    index = {:sxy, 0, d, 0}
+
+    assert adjacents(index, d).nw == {:sxy, 0, d - 1, 0}
+    assert adjacents(index, d).w == {:sxy, 0, d - 1, 1}
+    assert adjacents(index, d).sw == {:sxy, 0, d, 1}
+    assert adjacents(index, d).se == {:sxy, 0, d + 1, 0}
+    assert adjacents(index, d).e == {:sxy, 1, 1, d - 1}
+    assert adjacents(index, d).ne == {:sxy, 1, 0, d - 1}
+  end
+
+  test "links fields on the southeastern edge" do
+    d = 3
+    dy = d
+    dx = d * 2
+    index = {:sxy, 0, dx - 1, 1}
+
+    assert adjacents(index, d).nw == {:sxy, 0, dx - 2, 1}
+    assert adjacents(index, d).w == {:sxy, 0, dx - 2, 2}
+    assert adjacents(index, d).sw == {:sxy, 0, dx - 1, 2}
+    assert adjacents(index, d).se == {:sxy, 1, dx / 2 + 1, dy - 1}
+    assert adjacents(index, d).e == {:sxy, 1, dx / 2, dy - 1}
+    assert adjacents(index, d).ne == {:sxy, 0, dx - 1, 0}
+  end
+
+  test "links internal fields" do
+    d = 3
+    index = {:sxy, 0, d, 1}
+
+    assert adjacents(index, d).nw == {:sxy, 0, d - 1, 1}
+    assert adjacents(index, d).w == {:sxy, 0, d - 1, 2}
+    assert adjacents(index, d).sw == {:sxy, 0, d, 2}
+    assert adjacents(index, d).se == {:sxy, 0, d + 1, 1}
+    assert adjacents(index, d).e == {:sxy, 0, d + 1, 0}
+    assert adjacents(index, d).ne == {:sxy, 0, d, 0}
+  end
+end
