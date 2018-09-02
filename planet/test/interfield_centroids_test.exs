8c51ecedb8fffa5dde7f8d95f4c71e48831008b9
Will Shown <w@willshown.com>
2018-09-01 19:23:31 -0500

    Test error.

--- planet/test/interfield_centroids_test.exs
+++ planet/test/interfield_centroids_test.exs
@@ -8,8 +8,20 @@ defmodule PLANET.Geometry.InterfieldCentroidsTest do
   test "computes interfield centroids" do
     interfield_centroid_sphere = interfield_centroids(3)
 
-    # Check that we can access centroids from any 3 indexes of any order
+    # Check that we can access centroids from indexes in any order of 3 mutually adjacent fields
 
+    # Not mutually adjacent fields
+    assert :error =
+             Map.fetch(
+               interfield_centroid_sphere,
+               MapSet.new([
+                 :north,
+                 {:sxy, 0, 2, 0},
+                 {:sxy, 1, 0, 0}
+               ])
+             )
+
+    # Usual order
     assert {:ok, {:pos, c1_lat, c1_lon}} =
              Map.fetch(
                interfield_centroid_sphere,
@@ -20,6 +32,7 @@ defmodule PLANET.Geometry.InterfieldCentroidsTest do
                ])
              )
 
+    # Shuffled order
     assert {:ok, {:pos, c2_lat, c2_lon}} =
              Map.fetch(
                interfield_centroid_sphere,
@@ -31,7 +44,6 @@ defmodule PLANET.Geometry.InterfieldCentroidsTest do
              )
 
     # Assert that the accessed centroids are identical
-
     assert c1_lat == c2_lat
     assert c1_lon == c2_lon
   end
