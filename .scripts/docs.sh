3c6475f4bff6ae231d0a2e6069e707d026962522
Will Shown <w@willshown.com>
2019-03-01 16:12:12 -0500

    Build docs and move them into place on placard:build.

--- .scripts/docs.sh
+++ .scripts/docs.sh
@@ -1,11 +1,14 @@
-MIX_ENV=test
+rm -rf placard/functions/app/public/docs
+mkdir placard/functions/app/public/docs
 
 echo "Generating docs for GEOF.Planet"
 cd planet
 mix docs
 cd ..
+cp -r planet/doc placard/functions/app/public/docs/planet
 
 echo "Generating docs for GEOF.Shapes"
 cd shapes
 mix docs
 cd ..
+cp -r shapes/doc placard/functions/app/public/docs/shapes
