94fc0f5124fb7295f9dcab472c74ec7e70479198
Will Shown <w@willshown.com>
2019-03-24 22:15:04 -0500

    Add shallow water defaults

--- climate/lib/models/shallow_water.ex
+++ climate/lib/models/shallow_water.ex
@@ -1,8 +1,29 @@
 defmodule GEOF.Climate.ShallowWaterModel do
   @moduledoc """
     Solves the shallow water equations across the sphere.
+    Based on the Spherepack algorithm in Fortran by John Adams and Paul Swarztrauber,
+    retrieved from github.com:jlokimlin/spherepack
   """
 
-  def init_sphere do
-  end
+  import :math
+
+  @deg_to_rad pi() / 180
+
+  @sphere_defaults %{
+    # planet radius = earth's
+    aa: 6.37122e6,
+    # rate of rotation = earth's
+    omega: 7.292e-5,
+    # todo: what's this?
+    f_0: 2.0 * omega,
+    # todo: what's this?
+    u_0: 40.0,
+    # todo: what's this?
+    p_0: 2.94e4,
+    alpha: 60 * @deg_to_rad,
+    # time step in seconds
+    dt: 300.0
+  }
+
+  def sphere_defaults, do: @sphere_defaults
 end
